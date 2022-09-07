6157deb1abb04a04d648bb6ea62a4fb77c7a63cf
Baldur Kristinsson <bk@mbl.is>
2022-09-07 21:58:05 +0000

    initial wmk tap (v.0.9.8)

---
+++ Formula/wmk.rb
@@ -0,0 +1,22 @@
+class Wmk < Formula
+  desc "Flexible static site generator"
+  homepage "https://wmk.baldr.net/"
+  url "https://github.com/bk/wmk/archive/refs/tags/v0.9.8.tar.gz"
+  sha256 "030e6576be3da00e82e1c1d326f441a04bd125320a2e516e39d6ab6682493d23"
+  license "MIT"
+  depends_on "fswatch"
+  depends_on "python@3.10"
+  depends_on "rsync"
+
+  def install
+    ENV.deparallelize
+    prefix.install Dir["*"]
+    cd prefix
+    system "python3", "-m", "venv", "venv"
+    system "venv/bin/pip", "install", "-r", "requirements.txt"
+  end
+
+  test do
+    system "wmk", "--version"
+  end
+end
---
+++ readme.md
@@ -0,0 +1,24 @@
+# Homebrew tap for wmk
+
+To install [wmk](https://github.com/bk/wmk) using Homebrew, first add the tap:
+
+```bash
+brew tap bk/wmk
+```
+
+...and install it:
+
+```bash
+brew install wmk
+```
+
+Now this you should be able to use the `wmk` command normally.
+
+For further information, see the [wmk documentation](https://wmk.baldr.net/).
+
+## Uninstall
+
+```php
+brew uninstall wmk
+```
+
