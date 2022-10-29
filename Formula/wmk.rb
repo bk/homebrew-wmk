839332101ecce6a7a655519d9d89f2671df58018
Baldur Kristinsson <bk@mbl.is>
2022-10-29 16:25:54 +0000

    Updated formula for v0.9.11

--- Formula/wmk.rb
+++ Formula/wmk.rb
@@ -1,10 +1,10 @@
 class Wmk < Formula
   desc "Flexible static site generator"
   homepage "https://wmk.baldr.net/"
-  url "https://github.com/bk/wmk/archive/refs/tags/v0.9.10.tar.gz"
-  sha256 "046564c8fa332cb46bd27249a2a2a79ca8601dab67b74615a447e32675e8a309"
+  url "https://github.com/bk/wmk/archive/refs/tags/v0.9.11.tar.gz"
+  sha256 "1d7d6828f9b13df8cdca06648c21662abc00a219e2ccf75d9b2ce610705d665e"
   license "MIT"
-  version "0.9.10"
+  version "0.9.11"
   depends_on "fswatch"
   depends_on "python@3.10"
   depends_on "rsync"
